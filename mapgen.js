5700b9623a33c157a952c44966ec7a6542068496
Tom Käppler <45234980+TKSpectro@users.noreply.github.com>
2023-04-29 15:35:23 +0200

    add multiple map mode

--- mapgen.js
+++ mapgen.js
@@ -1,9 +1,7 @@
 const { writeFileSync } = require("fs");
 const convert = require("xml-js");
 
-const map = require("./map-large.js");
-
-const entities = [];
+const mapFileNames = ["map-small", "map-large"];
 
 const TYPE = {
   CHARACTER: 0,
@@ -29,68 +27,74 @@ const logic = (canMove, canCollide, isFinish) => {
   };
 };
 
-for (let y = 0; y < map.length; y++) {
-  for (let x = 0; x < map[y].length; x++) {
-    if (map[y][x] === 1) {
-      entities.push({
-        _attributes: {
-          name: `wall_${x}_${y}`,
-          "meta-entity": "ground_stone",
-        },
-        data: data(x, y, TYPE.GROUND),
-        logic: logic(false, true, false),
-      });
-    }
-    if (map[y][x] === 2) {
-      entities.push({
-        _attributes: {
-          name: `ground_${x}_${y}`,
-          "meta-entity": "dirt",
-        },
-        data: data(x, y, TYPE.GROUND),
-        logic: logic(false, true, false),
-      });
-    }
-    if (map[y][x] === 3) {
-      entities.push({
-        _attributes: {
-          name: `finish_${x}_${y}`,
-          "meta-entity": "finish",
-        },
-        data: data(x, y, TYPE.FINISH),
-        logic: logic(false, true, true),
-      });
-    }
-    if (map[y][x] === 4) {
-      entities.push({
-        _attributes: {
-          name: `coin_${x}_${y}`,
-          "meta-entity": "coin",
-        },
-        data: data(x, y, TYPE.COIN),
-        logic: logic(false, true, false),
-      });
-    }
-    if (map[y][x] === 9) {
-      entities.push({
-        _attributes: {
-          name: `player`,
-          "meta-entity": "player",
-        },
-        data: data(x, y, TYPE.CHARACTER),
-        logic: logic(true, true, false),
-      });
+mapFileNames.forEach((fileName) => {
+  const map = require(`./${fileName}.js`);
+
+  const entities = [];
+
+  for (let y = 0; y < map.length; y++) {
+    for (let x = 0; x < map[y].length; x++) {
+      if (map[y][x] === 1) {
+        entities.push({
+          _attributes: {
+            name: `wall_${x}_${y}`,
+            "meta-entity": "ground_stone",
+          },
+          data: data(x, y, TYPE.GROUND),
+          logic: logic(false, true, false),
+        });
+      }
+      if (map[y][x] === 2) {
+        entities.push({
+          _attributes: {
+            name: `ground_${x}_${y}`,
+            "meta-entity": "dirt",
+          },
+          data: data(x, y, TYPE.GROUND),
+          logic: logic(false, true, false),
+        });
+      }
+      if (map[y][x] === 3) {
+        entities.push({
+          _attributes: {
+            name: `finish_${x}_${y}`,
+            "meta-entity": "finish",
+          },
+          data: data(x, y, TYPE.FINISH),
+          logic: logic(false, true, true),
+        });
+      }
+      if (map[y][x] === 4) {
+        entities.push({
+          _attributes: {
+            name: `coin_${x}_${y}`,
+            "meta-entity": "coin",
+          },
+          data: data(x, y, TYPE.COIN),
+          logic: logic(false, true, false),
+        });
+      }
+      if (map[y][x] === 9) {
+        entities.push({
+          _attributes: {
+            name: `player`,
+            "meta-entity": "player",
+          },
+          data: data(x, y, TYPE.CHARACTER),
+          logic: logic(true, true, false),
+        });
+      }
     }
   }
-}
 
-let xmlDataStr = convert.js2xml(
-  { entities: { entity: entities } },
-  {
-    compact: true,
-    ignoreComment: true,
-    spaces: 2,
-  }
-);
+  const xmlDataStr = convert.js2xml(
+    { entities: { entity: entities } },
+    {
+      compact: true,
+      ignoreComment: true,
+      spaces: 2,
+    }
+  );
 
-writeFileSync("map.xml", xmlDataStr);
+  writeFileSync(`${fileName}.xml`, xmlDataStr);
+});
